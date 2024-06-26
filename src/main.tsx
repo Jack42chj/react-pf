a05696a101fecc1b5e5996e70a1a039acafe9c15
Jack42chj <hojinch99@gmail.com>
2024-06-26 20:18:39 +0900

    Vercel Analytics Added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
         <App />
+        <Analytics />
     </QueryClientProvider>
 );
