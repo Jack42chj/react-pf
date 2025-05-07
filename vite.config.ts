7ec5d4d9491fd65a36b3ec859b5d1bad35cbe9dc
CHOI HOJIN <hojinch99@gmail.com>
2025-05-07 23:24:50 +0900

    settings: viteConfig 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import babelConfig from "./babel.config";
+
+export default defineConfig(() => {
+  const PORT = 3044;
+
+  return {
+    plugins: [
+      react({ babel: babelConfig }),
+      tsconfigPaths(),
+      checker({
+        typescript: true,
+        eslint: {
+          useFlatConfig: true,
+          lintCommand: "eslint './src/**/*.{ts,tsx}'",
+          dev: { logLevel: ["error"] },
+        },
+      }),
+    ],
+    server: { port: PORT, host: true },
+  };
+});
